55a87d5e408cf99558486277eedb5a8a2e060ba6
Hossam Maarek <hosammostafa@yahoo.com>
2017-01-03 07:16:34 +0000

    testing change in schema.rb. to see difference in branching

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
+#Master Branch
 ActiveRecord::Schema.define(version: 20161224111900) do
 
   create_table "backhauls", force: :cascade do |t|
